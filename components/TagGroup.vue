3ade57a7027474dbc150908521e09aeee9a47518
takusan23 <takumu1200@gmail.com>
2020-06-16 00:23:22 +0900

    記事書いた

--- components/TagGroup.vue
+++ components/TagGroup.vue
@@ -1,21 +1,24 @@
 // タグのChipまとめたやつ
 
 <template>
-  <tbody>
-    <v-chip
-      class="ma-1 pa-4"
-      color="indigo"
-      text-color="white"
-      v-for="tag in tags"
-      :key="tag"
-      :to="`../../../posts/tag/${tag}/`"
-    >
-      <v-avatar left>
-        <v-icon>mdi-tag-text-outline</v-icon>
-      </v-avatar>
-      {{tag}}
-    </v-chip>
-  </tbody>
+  <div>
+    <client-only>
+      <!-- なんかしらんけどClientとServerでDOMが違うよエラー出るようになっちゃった。まあclient-onlyしとくか。 -->
+      <v-chip
+        v-for="tag in tags"
+        :key="tag"
+        class="ma-1 pa-4"
+        color="indigo"
+        text-color="white"
+        :to="`../../../posts/tag/${tag}/`"
+      >
+        <v-avatar left>
+          <v-icon>mdi-tag-text-outline</v-icon>
+        </v-avatar>
+        {{tag}}
+      </v-chip>
+    </client-only>
+  </div>
 </template>
 
 <script>
